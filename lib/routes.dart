b5e6c65ae7c0a91cfc5aa2c9c11e8f700f417e6c
trylix <brendensond@gmail.com>
2020-02-20 21:49:55 -0300

    Estrutura inicial do projeto

---
+++ lib/routes.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/widgets.dart';
+import 'package:nubank/src/screens/home.dart';
+
+final Map<String, WidgetBuilder> routes = <String, WidgetBuilder> {
+  '/': (BuildContext context) => HomeScreen(),
+};
---
+++ lib/src/themes/styles.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+ThemeData appTheme() {
+  return ThemeData(
+    primaryColor: Color(0xFF83259F),
+  );
+}
